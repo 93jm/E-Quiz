ce34a65c39be1faf2239dbc02fb678ea5a13e476
Evan <93_jm@naver.com>
2024-03-15 14:26:40 +0900

    fetch url 변경

--- src/app/_lib/getQuizItems.ts
+++ src/app/_lib/getQuizItems.ts
@@ -1,7 +1,7 @@
 export async function getQuizItems() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_QUIZ}`).then((res) =>
-    res.json()
-  );
+  const data = await fetch(
+    "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
+  ).then((res) => res.json());
 
   return data;
 }
